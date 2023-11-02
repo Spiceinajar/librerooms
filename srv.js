20bc88a52907c0b80ad889cade2f57dd595573a4
Spiceinajar <89741444+Spiceinajar@users.noreply.github.com>
2023-11-02 08:24:59 -0400

    Update srv.js

--- srv.js
+++ srv.js
@@ -455,7 +455,7 @@ async function run() {
                       }
 
                       if (cmd === "GETLOGIN") { //I made this command for the people who have lost their logins. Only administrators can use this command.
-                        sysMessage("KEY: " + dat.collections.users[args.user].password, parsed.room) //Bear in mind this will only be sent in private rooms and be deleted afterwards, I will make sure of that since I am the only administrator.
+                        sysMessage("KEY: " + dat.collections.users[args.user].key, parsed.room) //Bear in mind this will only be sent in private rooms and be deleted afterwards, I will make sure of that since I am the only administrator.
                       }
                     }
         
